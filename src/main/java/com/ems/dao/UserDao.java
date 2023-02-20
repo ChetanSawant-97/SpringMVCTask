0c87a49656a30bcd9eaa5b1531e3d0ca8584da49
Chetan Sawant <79401326+ChetanSawant-97@users.noreply.github.com>
2023-02-20 17:20:23 +0530

    Update UserDao.java

--- src/main/java/com/ems/dao/UserDao.java
+++ src/main/java/com/ems/dao/UserDao.java
@@ -2,9 +2,7 @@ package com.ems.dao;
 
 import java.util.List;
 import java.util.Optional;
-
 import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Component;
